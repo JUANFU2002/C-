0b32e40be38b855e87b424470803fcd8b45c4ef0
卷福潘尔摩斯 <82427496+JUANFU2002@users.noreply.github.com>
2021-10-16 17:43:40 +0800

    Add files via upload

---
+++ pzj1.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	printf("HELLOW WORLD\n");
+	printf("HELLOW WORLD\n");
+	printf("HELLOW WORLD\n");
+	printf("HELLOW WORLD\n");
+	printf("HELLOW WORLD\n");
+	printf("HELLOW WORLD\n");
+	return 0;
+}
