50e527814aff49ecae7bb6d7b2b16d734066da7b
卷福潘尔摩斯 <82427496+JUANFU2002@users.noreply.github.com>
2021-10-20 16:42:58 +0800

    Add files via upload

---
+++ pzj-5.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+   /* int a;
+    int b;
+    int m;
+    int n;
+    scanf("%d %d", &a, &b);
+    m = a / b;
+    n = a % b;
+    printf("%d %d", m, n);*/
+   /* int a = 0;
+    int b = 0;
+    scanf("%d %d", &a, &b);
+    int m = a / b;
+    int n = a % b;
+    printf("%d %d", m, n);*/
+    int a = 40;
+    int c = 212;
+    int m = 0;
+    m = (-8 + 22) * a - 10 + c / 2;
+    printf("%d", m);
+	return 0;
+}
