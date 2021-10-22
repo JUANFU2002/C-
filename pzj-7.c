09ab6a15cd041b577ae28ab6729dca28b92323ba
卷福潘尔摩斯 <82427496+JUANFU2002@users.noreply.github.com>
2021-10-22 23:39:08 +0800

    Add files via upload

---
+++ pzj-7.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int fun(int x)
+//{
+//	if (x < 0)
+//		return 1;
+//	else if (x > 0)
+//		return -1;
+//	else
+//		return 0;
+//}
+//int main()
+//{
+//	int x;
+//	scanf("%d", &x);
+//	int y = fun(x);
+//	printf("%d", y);
+//	return 0;
+//}
+//int main()
+//{
+//	int se;
+//	scanf("%d", &se);
+//	int a = se / 3600;
+//	int b = se % 3600 / 60;
+//	int c = se % 3600 % 60;
+//	printf("%d %d %d", a, b, c);
+//
+//
+//	return 0;
+//}
+int main()
+{
+
+	return 0;
+}
