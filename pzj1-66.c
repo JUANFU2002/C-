2a0451ce02427d4074aa8ace78b24a941061b009
卷福潘尔摩斯 <82427496+JUANFU2002@users.noreply.github.com>
2021-12-30 15:02:04 +0800

    Add files via upload

---
+++ pzj1-66.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//æÿ’Ûµπ÷√
+int main()
+{
+	int j, k, x;
+	int a[3][3] = { 9,7,5,3,1,2,4,6,8 };
+	for (j = 0; j < 3; j++)
+	{
+		for (k = j + 1; k < 3; k++)
+		{
+			x = a[j][k];
+			a[j][k] = a[k][j];
+			a[k][j] = x;
+		}
+	}
+	for (j = 0; j < 3; j++)
+	{
+		for (k = 0; k < 3; k++)
+		{
+			printf("%d ", a[j][k]);
+		}
+	}
+	return 0;
+}
